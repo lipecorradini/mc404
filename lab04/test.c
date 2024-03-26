6911cd5ce7bd185077467a6803b11a303e095cd0
lipecorradini <luizfelipecorradini@gmail.com>
2024-03-26 16:25:07 -0300

    positivos funcionando

--- lab04/test.c
+++ lab04/test.c
@@ -9,8 +9,7 @@ int hx_to_dec(char *str)
     int curr_exp = 1;
     int arr_size = (sizeof(str) - 2 * sizeof(char)) / (sizeof(char));
 
-
-    for (int i = 1; i < arr_size; i++) 
+    for (int i = 1; i < arr_size; i++)
         curr_exp *= 16;
 
     for (int i = 2; i < arr_size + 2; i++)
@@ -36,34 +35,119 @@ int hx_to_dec(char *str)
     return int_dec;
 }
 
-void dec_to_bin(int n, char *bin){
-    
-    int tam = 0;
-    int new = n;
-    while(new/2 != 0){
-        tam ++;
-        new /= 2;
+int size_string(char *str)
+{
+
+    int i = 0;
+    for (i = 0; str[i] != '\n'; i++)
+        ;
+    return i;
+}
+
+int size_base(int n, int base)
+{
+    int tam = 0, new = n;
+    while (new != 0)
+    {
+        tam++;
+        new /= base;
     }
 
+    return tam;
+}
+
+void neg_dec_to_bin(int n, char *str)
+{
+
+    char *bin[33];
+    printf("aa\n");
+    dec_to_base(n, bin, 2);
+
+    printf("aa\n");
+    int tamanho = size_string(bin);
+    int i, carry = 1;
+
+    str[tamanho - 1] = '\0';
+
+    for (i = tamanho - 1; i >= 2; i--)
+    {
+        if (bin[i] == '1' && carry == 1)
+        {
+            str[i] = '0';
+        }
+        else if (bin[i] == '0' && carry == 1)
+        {
+            str[i] = '1';
+            carry = 0;
+        }
+        else
+        {
+            str[i] = bin[i];
+        }
+    }
+}
+
+void dec_to_base(int n, char *str, int base)
+{
+
+    int tam = size_base(n, base);
+
     // para números positivos
-    printf("tamanho  %d\n", tam);
     int i = 0;
 
-    while(n/2 != 0){
-        int res = n % 2;
-        bin[tam - i + 2] = res + '0';
-        printf("index: %d\n", tam - i + 1);
-        n /= 2;
+    while (n != 0)
+    {
+        int res = n % base;
+        // printf("%d resto por %d = %d\n", n, base, res);
+        str[tam - i + 1] = res + '0';
+        n /= base;
         i++;
     }
 
-    bin[tam] = '\n';
+    // printf("pen: %c\n", str[tam+1]);
+    str[tam + 2] = '\n';
+    str[0] = '0';
 
-    bin[0] = '0';
-    bin[1] = 'b';
-    return ;
+    switch (base)
+    {
+    case 2:
+        str[1] = 'b';
+        break;
+    case 8:
+        str[1] = 'o';
+        break;
+    case 16:
+        str[1] = 'x';
+        break;
 
+    default:
+        break;
+    }
+
+    return;
 }
+
+void pr_str(char *str)
+{
+    int tam = size_string(str);
+    for (int i = 0; i < tam; i++)
+    {
+        printf("%c", str[i]);
+    }
+    printf("\n");
+}
+
+int atoi(char *arr)
+{
+    int result = 0;
+    int size = size_string(arr);
+    for (int i = 1; i < size; i++)
+    {
+        result = result * 10 + (arr[i] - '0');
+    }
+    return result;
+}
+
 int main()
 {
     char str[20];
@@ -77,9 +161,26 @@ int main()
         printf("decimal: %d\n", dec);
 
         char bin[32];
-        dec_to_bin(dec, bin);
-        printf("%s\n", bin);
+        dec_to_base(dec, bin, 2);
+        pr_str(bin);
+
+        char hex[20];
+        dec_to_base(dec, hex, 16);
+        pr_str(hex);
+
+        char oct[20];
+        dec_to_base(dec, oct, 8);
+        pr_str(oct);
+
         break;
+
+    case '-':
+        char neg_bin[33];
+        int neg_dec = atoi(str);
+        printf("%d\n", neg_dec);
+        printf("%d\n", neg_dec);
+        neg_dec_to_bin(neg_dec, neg_bin);
+        pr_str(neg_bin);
 
     default:
         // tratar como decimal
