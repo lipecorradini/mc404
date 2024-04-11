99b0091d272f908d6f0a5fad8e7a1580492574b1
ra230613 <l230613@dac.unicamp.br>
2024-04-11 15:48:37 -0300

    finalizada

--- lab05/lab05.c
+++ lab05/lab05.c
@@ -1,58 +1,78 @@
-#include <stdio.h>
 
 #define getBit(w, i) ((w >> i) & 1) // int w e index i
 #define MASK(j) (1<<j) // peja o j-ésimo bit, so fazer um and pra verificar se é 1
 
-void pack(int input, int start_bit, int end_bit, int packed){
-    
-    // iterar pelos índices de comeco em fim no input
-    // adicionar o valor à packed se for 1
-    // retornar packed (fazer para todos os int)
+#define STDIN_FD  0
+#define STDOUT_FD 1
 
+
+int read(int __fd, const void *__buf, int __n){
+    int ret_val;
+  __asm__ __volatile__(
+    "mv a0, %1           # file descriptor\n"
+    "mv a1, %2           # buffer \n"
+    "mv a2, %3           # size \n"
+    "li a7, 63           # syscall write code (63) \n"
+    "ecall               # invoke syscall \n"
+    "mv %0, a0           # move return value to ret_val\n"
+    : "=r"(ret_val)  // Output list
+    : "r"(__fd), "r"(__buf), "r"(__n)    // Input list
+    : "a0", "a1", "a2", "a7"
+  );
+  return ret_val;
 }
 
-int power(int num, int exp)
+void write(int __fd, const void *__buf, int __n)
 {
-    if (exp == 0)
-        return 1;
-    int result = 1;
-    for (int i = 0; i < exp; i++)
-    {
-        result *= num;
-    }
-    return result;
+  __asm__ __volatile__(
+    "mv a0, %0           # file descriptor\n"
+    "mv a1, %1           # buffer \n"
+    "mv a2, %2           # size \n"
+    "li a7, 64           # syscall write (64) \n"
+    "ecall"
+    :   // Output list
+    :"r"(__fd), "r"(__buf), "r"(__n)    // Input list
+    : "a0", "a1", "a2", "a7"
+  );
 }
 
-int bin_to_dec(char *str)
+void exit(int code)
+{
+  __asm__ __volatile__(
+    "mv a0, %0           # return code\n"
+    "li a7, 93           # syscall exit (64) \n"
+    "ecall"
+    :   // Output list
+    :"r"(code)    // Input list
+    : "a0", "a7"
+  );
+}
+
+int concatenate(int *numbers){
+    
+    int final = 0;
+
+    final |= (numbers[0] & 0b11111) << 0;
+    final |= (numbers[1] & 0b1111111) << 5;
+    final |= (numbers[2] & 0b111111111) << 12;
+    final |= (numbers[3] & 0b1111) << 21;
+    final |= (numbers[4] & 0b1111111) << 25;
+
+    return final;
+
+}
+
+int dec_to_int(char *str)
 {
     int result = 0;
+    int exp = 1000;
+
     for (int i = 0; i < 4; i++)
     {
-        result += (int)(power(2, 3 - i) * (str[i] - '0'));
+        result += (str[i] - '0') * exp;
+        exp /= 10;
     }
     return result;
-
-}
-
-void printBits(int num) {
-    // Number of bits in an int
-    int numBits = sizeof(int) * 8;
-    
-    // Mask to check each bit
-    unsigned int mask = 1 << (numBits - 1);
-    
-    printf("Binary representation of %d: ", num);
-    for (int i = 0; i < numBits; ++i) {
-        // Check if the bit at position i is set
-        if (num & mask) {
-            printf("1");
-        } else {
-            printf("0");
-        }
-        // Shift the mask to the right by 1
-        mask >>= 1;
-    }
-    printf("\n");
 }
 
 int set_int(char *str, int start_index){
@@ -68,46 +88,61 @@ int set_int(char *str, int start_index){
 
     int cont = 0;
     for(int i = start_index + 1; i < start_index + 5; i++){
-        printf("%c\n", str[i]);
         buff[cont] = str[i];
         cont ++;
     }
     
     buff[5] = '\0';
 
-    return (signal == '+') ? bin_to_dec(buff) : bin_to_dec(buff) * -1;
+    return (signal == '+') ? dec_to_int(buff) : dec_to_int(buff) * -1;
 
+}
+
+void hex_code(int val){
+    char hex[11];
+    unsigned int uval = (unsigned int) val, aux;
+
+    hex[0] = '0';
+    hex[1] = 'x';
+    hex[10] = '\n';
+
+    for (int i = 9; i > 1; i--){
+        aux = uval % 16;
+        if (aux >= 10)
+            hex[i] = aux - 10 + 'A';
+        else
+            hex[i] = aux + '0';
+        uval = uval / 16;
+    }
+    write(1, hex, 11);
+    // printf("%s\n", hex);
 }
 
 int main(){
     
     char str[32];
 
-    scanf("%[^\n]%*c", str); // Temporário, para ler a string inteira
-    printf("string: %s\n", str);
+    read(STDIN_FD, str, 32);
+  
 
-    int first = set_int(str, 0);
-    int second = set_int(str, 6);
-    int third = set_int(str, 12);
-    int fourth = set_int(str, 18);
-    int fifth = set_int(str, 24);
+    int values[5];
 
-    printf("First: \n");
-    printBits(first);
-    printf("Second: \n");
-    printBits(second);
+    values[0] = set_int(str, 0);
+    values[1] = set_int(str, 6);
+    values[2] = set_int(str, 12);
+    values[3] = set_int(str, 18);
+    values[4] = set_int(str, 24);
 
 
+    int final_bin = concatenate(values);
 
-    // Ideia Geral:
-    // Receber a entrada como strings 
-    // Dividir como quatro inteiros sinalados
-        // Se for negativo, transformá-lo para complemento de dois
-     // Chamar a função pack para agrupá-los
-        // Colocá-los em suas devidas posições, shiftando os bits
-    // Transformar para Hexadecimal
+
+    hex_code(final_bin);
     
+}
 
-
-    
+void _start()
+{
+  int ret_code = main();
+  exit(ret_code);
 }
