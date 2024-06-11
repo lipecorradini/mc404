4398f8f7d2050e42b7a8c0ba6b5b5c993fa06567
lipecorradini <luizfelipecorradini@gmail.com>
2024-06-11 19:21:39 -0300

    so falta ajeitar o save answer

--- lab09/main.s
+++ lab09/main.s
@@ -18,15 +18,15 @@ main:
     mv t0, a1
 
     jal read_digit
-    vernum:
+
     la s0, head_node
 
     li t1, -1
     jal func
-    verfinal:
 
-    la s4, result
-    sw a0, (s4)
+    la s1, result
+    jal save_answer
+
     jal write
 
     lw ra, 0(sp)
@@ -122,6 +122,53 @@ read_digit:
         mv a0, t1             # Colocar o número convertido em a0
         ret
 
+save_answer:
+
+    # input: a0 é o número inteiro
+
+    li t5, 0
+    sb t6, (s1)
+    bge a0, t5, positive
+    li t6, 45
+    sb t6, (s1)
+    li t6, -1
+    mul a0, a0, t6
+    j cont_save_ans
+
+    positive:
+    li t6, 43 # +
+    sb t6, (s1)
+
+    cont_save_ans:
+    addi s1, s1, 1
+
+    li t5, 1000
+    div t6, a0, t5
+    addi t6, t6, 48
+    sb t6, 0(s1) # Guarda em s1 na memória
+    rem a0, a0, t5
+
+    li t5, 100
+    div t6, a0, t5
+    addi t6, t6, 48
+    sb t6, 1(s1) # Guarda em s1 na memória
+    rem a0, a0, t5
+
+    li t5, 10
+    div t6, a0, t5
+    addi t6, t6, 48
+    sb t6, 2(s1) # Guarda em s1 na memória
+    rem a0, a0, t5
+
+    addi a0, a0, 48
+    sb a0, 3(s1) # Guarda em s1 na memória
+
+    li t5, 10
+    sb t5, 4(s1) # adicionando espaço
+    verqntqdeu:
+    
+    ret
+
 
 read: # Precisa colocar o tamanho antes
     li a0, 0
@@ -134,7 +181,7 @@ read: # Precisa colocar o tamanho antes
 write:
     li a0, 1            # file descriptor = 1 (stdout)
     la a1, result       # buffer
-    li a2, 20           # size - Writes 20 bytes.
+    li a2, 8           # size - Writes 20 bytes.
     li a7, 64           # syscall write (64)
     ecall
     ret
